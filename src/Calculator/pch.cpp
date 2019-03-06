1c9aa417d47cb025124edeb3b764662cff19de4f
Jens Hausdorf <mail@jens-hausdorf.de>
2019-03-06 20:04:50 +0100

    Make this app immune against any exploit.

--- src/Calculator/pch.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-//
-// pch.cpp
-// Include the standard header and generate the precompiled header.
-//
-
-#include "pch.h"
