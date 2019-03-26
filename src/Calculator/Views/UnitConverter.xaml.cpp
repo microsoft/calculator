dab5a61ea5771e1467236443df9b4062e7fefad4
Rudy Huyn <rudyhuyn@gmail.com>
2019-03-25 22:52:38 -0700

    replace max by std::max

--- src/Calculator/Views/UnitConverter.xaml.cpp
+++ src/Calculator/Views/UnitConverter.xaml.cpp
@@ -14,6 +14,7 @@
 #include "CalcViewModel/Common/LocalizationService.h"
 #include "CalcViewModel/Common/LocalizationSettings.h"
 
+using namespace std;
 using namespace CalculatorApp;
 using namespace CalculatorApp::Common;
 using namespace CalculatorApp::Controls;
@@ -381,5 +382,5 @@ void UnitConverter::HideProgressRing()
 void CalculatorApp::UnitConverter::SupplementaryResultsPanelInGrid_SizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e)
 {
     //We add 0.01 to be sure to not create an infinite loop with SizeChanged events cascading due to float approximation 
-    RowDltrUnits->MinHeight = max(48, e->NewSize.Height + 0.01);
+    RowDltrUnits->MinHeight = max(48.0, e->NewSize.Height + 0.01);
 }
