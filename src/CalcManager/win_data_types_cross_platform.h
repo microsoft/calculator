49fd24e9620ac1bd50bd270b67fe2f4b7767aafd
fwcd <fwcdmail@gmail.com>
2019-03-27 23:49:02 +0100

    Remove unused data types from win_data_types_cross_platform.h

--- src/CalcManager/win_data_types_cross_platform.h
+++ src/CalcManager/win_data_types_cross_platform.h
@@ -1,26 +1,4 @@
 #pragma once
 
-#include <cstdint>
-
-typedef uint64_t ULONGLONG;
-typedef int32_t INT;
-typedef uint8_t CHAR;
-typedef int32_t LONG;
-typedef uint8_t BYTE;
-typedef uintptr_t UINT_PTR;
-typedef unsigned long ULONG_PTR;
-typedef uint32_t ULONG32;
-typedef uint32_t DWORD;
-typedef uint32_t ULONG;
-typedef uint16_t USHORT;
-typedef wchar_t WCHAR;
-typedef uint16_t WORD;
-typedef UINT_PTR WPARAM;
-typedef ULONG_PTR DWORD_PTR;
-
-#define LOWORD(dw) ((WORD)(((DWORD_PTR)(dw)) & 0xffff))
-#define HIWORD(dw) ((WORD)((((DWORD_PTR)(dw)) >> 16) & 0xffff))
 #define LODWORD(qw) ((DWORD)(qw))
 #define HIDWORD(qw) ((DWORD)(((qw) >> 32) & 0xffffffff))
-
-#define ARRAYSIZE(a) (sizeof(a) / sizeof(*a))
