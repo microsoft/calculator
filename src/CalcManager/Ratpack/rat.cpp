de553e6bc949b084e7dd155b5b4e43956e4b2f0e
Michał Janiszewski <janisozaur@users.noreply.github.com>
2019-04-01 15:13:29 -0400

    Update src/CalcManager/Ratpack/rat.cpp
    Co-Authored-By: ariccio <test35965@gmail.com>

--- src/CalcManager/Ratpack/rat.cpp
+++ src/CalcManager/Ratpack/rat.cpp
@@ -100,7 +100,6 @@ void fracrat( _Inout_ PRAT *pa , uint32_t radix, int32_t precision)
 //-----------------------------------------------------------------------------
 
 void mulrat( _Inout_ PRAT *pa, _In_ PRAT b, int32_t precision)
-    
     {
     // Only do the multiply if it isn't zero.
     if ( !zernum( (*pa)->pp ) )
