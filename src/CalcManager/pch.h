8279df69b936c3e98416479dd5516ab699dbe04a
Daniel Belcher <dabelc@microsoft.com>
2019-04-05 11:59:08 -0700

    - Move comment about pch use to pch for CalcManager.

--- src/CalcManager/pch.h
+++ src/CalcManager/pch.h
@@ -3,6 +3,11 @@
 
 #pragma once
 
+// The CalcManager project should be able to be compiled with or without a precompiled header
+// in - order to support other toolchains besides MSVC. When adding new system headers, make sure
+// that the relevant source file includes all headers it needs, but then also add the system headers
+// here so that MSVC users see the performance benefit.
+
 #include <algorithm>
 #include <array>
 #include <cassert>
