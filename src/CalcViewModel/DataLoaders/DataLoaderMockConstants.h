3593a9db02991fbac0f9a58e2a22e5e0586c44ed
Pepe Rivera <joriv@microsoft.com>
2019-05-23 14:22:07 -0700

    Remove unused var

--- src/CalcViewModel/DataLoaders/DataLoaderMockConstants.h
+++ src/CalcViewModel/DataLoaders/DataLoaderMockConstants.h
@@ -9,6 +9,5 @@ namespace CalculatorApp
     {
         static constexpr auto sc_MetadataUriLocalizeFor = L"https://go.microsoft.com/fwlink/?linkid=2091028&localizeFor=";
         static constexpr auto sc_RatiosUriRelativeTo = L"https://go.microsoft.com/fwlink/?linkid=2091307&localCurrency=";
-        static constexpr auto sc_ApiKey = L"";
     }
 }
