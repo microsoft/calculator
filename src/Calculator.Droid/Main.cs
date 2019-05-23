896d06494a617d73ce834c50b3d4927f127271a1
David Oliver <david.oliver@nventive.com>
2019-05-23 14:50:18 -0400

    [Android] Adjust namespace

--- src/Calculator.Droid/Main.cs
+++ src/Calculator.Droid/Main.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 using Com.Nostra13.Universalimageloader.Core;
 using Windows.UI.Xaml.Media;
 
-namespace WindowsCalculator.Droid
+namespace CalculatorApp.Droid
 {
 	[global::Android.App.ApplicationAttribute(
 		Label = "@string/ApplicationName",
