297df8b226927f9278aa6f14906048f08f7132d2
Maxime Dion <maxime.dion@nventive.com>
2019-05-24 14:10:07 -0400

    154463 [Calculator] SplashScreen + assets 154450 [Calculator] Correct app name and icon on iOS and Android

--- src/Calculator.iOS/Properties/AssemblyInfo.cs
+++ src/Calculator.iOS/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Uno Calculator")]
+[assembly: AssemblyTitle("Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
