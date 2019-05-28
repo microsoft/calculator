3797540db1cce9207a58bd26c2cc736a829885cd
Maxime Dion <maxime.dion@nventive.com>
2019-05-28 10:28:52 -0400

    Hide parenthesis count when it's zero

---
+++ src/Calculator.Shared/Converters/ZeroToEmptyStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace CalculatorApp.Converters
+{
+	class ZeroToEmptyStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string culture)
+		{
+			string myValue = value.ToString();
+
+			if (myValue == "0")
+			{
+				return "";
+			}
+			else
+			{
+				return value;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string culture)
+		{
+			return value;
+		}
+	}
+}
