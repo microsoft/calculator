f59a0044471c864797237898bd8b5721981dc051
Jerome Laban <jerome.laban@nventive.com>
2019-06-05 16:52:11 -0400

    Generate wasm .bc file for CalcManager

--- src/CalcManager/build.sh
+++ src/CalcManager/build.sh
@@ -1,3 +1,5 @@
+mkdir -p bin/wasm
+echo Generating WASM File
 emcc \
 	-std=c++17 \
 	-s WASM=1 \
@@ -6,6 +8,19 @@ emcc \
     -s ALLOW_MEMORY_GROWTH=1 \
 	-s BINARYEN=1 \
 	-s SIDE_MODULE=1 \
-	-o CalcManager.wasm \
+	-s DISABLE_EXCEPTION_CATCHING=0 \
+	-o bin/wasm/CalcManager.wasm \
 	-s EXPORT_ALL=1 \
+	CEngine/*.cpp Ratpack/*.cpp *.cpp -I.
+
+echo Generating LLVM Bitcode files
+emcc \
+	-std=c++17 \
+	-s WASM=1 \
+	-s LEGALIZE_JS_FFI=0 \
+	-s RESERVED_FUNCTION_POINTERS=64 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+	-s BINARYEN=1 \
+	-s DISABLE_EXCEPTION_CATCHING=0 \
+	-o bin/wasm/CalcManager.bc \
 	CEngine/*.cpp Ratpack/*.cpp *.cpp -I.
