da9f4ea85637c0366a35db059b750403dd53dd00
Michał Janiszewski <janisozaur@users.noreply.github.com>
2019-06-18 02:10:15 +0200

    Fix type of m_openParenCount (#550)
    m_openParenCount is always used as unsigned and compared against unsigneds

--- src/CalcManager/CEngine/scicomm.cpp
+++ src/CalcManager/CEngine/scicomm.cpp
@@ -373,7 +373,8 @@ void CCalcEngine::ProcessCommandWorker(OpCode wParam)
         m_lastVal = 0;
 
         m_bChangeOp = false;
-        m_precedenceOpCount = m_nTempCom = m_nLastCom = m_nOpCode = m_openParenCount = 0;
+        m_openParenCount = 0;
+        m_precedenceOpCount = m_nTempCom = m_nLastCom = m_nOpCode = 0;
         m_nPrevOpCode = 0;
         m_bNoPrevEqu = true;
 
