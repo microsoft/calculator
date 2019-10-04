3ece401b6d1cc6078bdc2b9ba7448ea32350e50f
David Shoemaker <dashoe@microsoft.com>
2019-10-04 14:53:43 -0700

    Fix bug where active tracing would turn back on after clicking button to turn it off.

--- src/Calculator/Views/GraphingCalculator/GraphingCalculator.xaml.h
+++ src/Calculator/Views/GraphingCalculator/GraphingCalculator.xaml.h
@@ -57,6 +57,8 @@ namespace CalculatorApp
         void OnActiveTracingClick(Platform::Object ^ sender, Windows::UI::Xaml::RoutedEventArgs ^ e);
         void OnGraphLostFocus(Platform::Object ^ sender, Windows::UI::Xaml::RoutedEventArgs ^ e);
         void OnLoosingFocus(Windows::UI::Xaml::UIElement ^ sender, Windows::UI::Xaml::Input::LosingFocusEventArgs ^ args);
+
+        bool ActiveTracingOn;
     };
 
 }
