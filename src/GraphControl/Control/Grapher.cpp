885871a9812d42087dc0c29425304631501fc333
sanderl <46726333+sanderl@users.noreply.github.com>
2019-11-08 14:53:18 -0800

    Moved formatoptions logic to the Grapher constructor and reverted LineColor.Text resource that was mistakenly taken out

--- src/GraphControl/Control/Grapher.cpp
+++ src/GraphControl/Control/Grapher.cpp
@@ -79,6 +79,8 @@ namespace GraphControl
         auto cw = CoreWindow::GetForCurrentThread();
         cw->KeyDown += ref new TypedEventHandler<CoreWindow ^, KeyEventArgs ^>(this, &Grapher::OnCoreKeyDown);
         cw->KeyUp += ref new TypedEventHandler<CoreWindow ^, KeyEventArgs ^>(this, &Grapher::OnCoreKeyUp);
+
+                auto& formatOptions = m_solver->FormatOptions();
     }
 
     void Grapher::OnLoaded(Object ^ sender, RoutedEventArgs ^ args)
@@ -297,6 +299,7 @@ namespace GraphControl
 
     void Grapher::UpdateGraph()
     {
+        auto& formatOptions = m_solver->FormatOptions();
         if (m_renderMain && m_graph != nullptr)
         {
             auto validEqs = GetValidEquations();
@@ -325,6 +328,7 @@ namespace GraphControl
                 {
                     if (m_graph->TryInitialize(graphExpression.get()))
                     {
+                        auto& formatOptions2 = m_solver->FormatOptions();
                         UpdateGraphOptions(m_graph->GetOptions(), validEqs);
                         SetGraphArgs();
 
