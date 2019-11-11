44f9f42f144274788707771164214a232d7dc64d
sanderl <46726333+sanderl@users.noreply.github.com>
2019-11-11 14:34:02 -0800

    rebase key-graph-features and fix issue where removing an equation box and adding a new one repopulates the previous equation

--- src/Calculator/Converters/IsLineEnabledToOpacityConverter.cpp
+++ src/Calculator/Converters/IsLineEnabledToOpacityConverter.cpp
@@ -15,7 +15,7 @@ Object ^ IsLineEnabledToOpacityConverter::Convert(Object ^ value, TypeName /*tar
     auto boxedBool = dynamic_cast<Box<bool> ^>(value);
     auto boolValue = (boxedBool != nullptr && boxedBool->Value);
 
-    return boolValue ? 1.0 : 0.4;
+    return boolValue ? 1.0 : 0.7;
 }
 
 Object ^ IsLineEnabledToOpacityConverter::ConvertBack(Object ^ value, TypeName targetType, Object ^ parameter, String ^ language)
