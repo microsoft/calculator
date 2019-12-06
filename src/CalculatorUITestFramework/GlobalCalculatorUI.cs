b5dee8844fab3303a622581c2696938e05a7bb42
Crystal Edwards <45952655+v-credwa@users.noreply.github.com>
2019-12-06 13:48:01 -0800

    - In test framework, where it worked, made some elements private - Created EnsureCalculatorHasFocus() in GlobalCalculatorUI.cs - Due to new classes, moved some methods to more correct classes - Moved "page.MemoryPanel.ResizeWindowToDiplayMemoryLabel();" from SetupCalculatorSession() to ClassInitialize()

--- src/CalculatorUITestFramework/GlobalCalculatorUI.cs
+++ src/CalculatorUITestFramework/GlobalCalculatorUI.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 using OpenQA.Selenium.Appium.Windows;
+using System.Drawing;
 
 namespace CalculatorUITestFramework
 {
@@ -10,13 +11,10 @@ namespace CalculatorUITestFramework
     public class GlobalCalculatorUI
     {
         private WindowsDriver<WindowsElement> session => WinAppDriver.Instance.CalculatorSession;
-        public WindowsElement CalculatorExpression => this.session.TryFindElementByAccessibilityId("CalculatorExpression");
         public WindowsElement Header => this.session.TryFindElementByAccessibilityId("Header");
         public WindowsElement Minimize => this.session.TryFindElementByAccessibilityId("Minimize");
         public WindowsElement Maximize => this.session.TryFindElementByAccessibilityId("Maximize");
-        public WindowsElement AppName => this.session.TryFindElementByAccessibilityId("AppName");
-        public WindowsElement ScrollLeft => this.session.TryFindElementByAccessibilityId("CalculationResultScrollLeft");
-        public WindowsElement ScrollRight => this.session.TryFindElementByAccessibilityId("CalculationResultScrollRight");
+        private WindowsElement AppName => this.session.TryFindElementByAccessibilityId("AppName");
         public WindowsElement Window => this.session.FindElementByClassName("Windows.UI.Core.CoreWindow");
 
         /// <summary>
@@ -27,13 +25,34 @@ namespace CalculatorUITestFramework
         {
             return this.Header.Text;
         }
-        /// <summary>
-        /// Gets the text from the Calculator Expression control and removes narrator text that is not displayed in the UI.
-        /// </summary>
-        /// <returns>The string shown in the UI.</returns>
-        public string GetCalculatorExpressionText()
+        ///// <summary>
+        ///// Gets the calculators window's X and Y position and returns it as a string. e.g. "(0, 0)"
+        ///// </summary>
+        public string GetCalculatorWindowPosition()
         {
-            return this.CalculatorExpression.Text.Replace("Expression is", string.Empty).Trim();
+            Point getCalculatorWindowPosition = WinAppDriver.Instance.CalculatorSession.Manage().Window.Position;
+            int X = getCalculatorWindowPosition.X;
+            int Y = getCalculatorWindowPosition.Y;
+            var CalculatorWindowPosition = (X, Y);
+            return CalculatorWindowPosition.ToString();
+        }
+        ///// <summary>
+        ///// Gets the calculators window's width and height and returns it as a string. e.g. "(322, 396)"
+        ///// </summary>
+        public string GetCalculatorWindowSize()
+        {
+            Size getCalculatorWindowSize = WinAppDriver.Instance.CalculatorSession.Manage().Window.Size;
+            int Width = getCalculatorWindowSize.Width;
+            int Height = getCalculatorWindowSize.Height;
+            var CalculatorWindowSize = (Width, Height);
+            return CalculatorWindowSize.ToString();
+        }
+        ///// <summary>
+        ///// Clicks the AppName element on Windows Calculator to ensure that the app has focus
+        ///// </summary>
+        public void EnsureCalculatorHasFocus()
+        {
+            this.AppName.Click();
         }
     }
 
