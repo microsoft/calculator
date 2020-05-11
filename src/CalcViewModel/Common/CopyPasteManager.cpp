0810a15c05738fb20741c841ce6ecad857311ae6
Eric Wong (PAX) <eriwong@microsoft.com>
2020-05-11 16:51:26 -0700

    handle other group characters

--- src/CalcViewModel/Common/CopyPasteManager.cpp
+++ src/CalcViewModel/Common/CopyPasteManager.cpp
@@ -598,6 +598,7 @@ ULONG32 CopyPasteManager::ProgrammerOperandLength(Platform::String ^ operand, Nu
 Platform::String ^ CopyPasteManager::RemoveUnwantedCharsFromString(Platform::String ^ input)
 {
     constexpr wchar_t unWantedChars[] = { L' ', L',', L'"', 165, 164, 8373, 36, 8353, 8361, 8362, 8358, 8377, 163, 8364, 8234, 8235, 8236, 8237, 160 };
+    input = CalculatorApp::Common::LocalizationSettings::GetInstance().RemoveGroupSeparators(input);
     return ref new String(Utils::RemoveUnwantedCharsFromString(input->Data(), unWantedChars).c_str());
 }
 
