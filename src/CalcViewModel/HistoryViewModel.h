e71da84625c08d5bc77a9b93e7d7bb3c966e6cfb
Rudy Huyn <rudyhuyn@gmail.com>
2020-05-11 21:47:26 -0700

    Remove friend class in HistoryViewModel

--- src/CalcViewModel/HistoryViewModel.h
+++ src/CalcViewModel/HistoryViewModel.h
@@ -61,8 +61,6 @@ namespace CalculatorApp
             CalculatorDisplay m_calculatorDisplay;
             CalculationManager::CalculatorMode m_currentMode;
             Platform::String ^ m_localizedHistoryCleared;
-
-            friend class CalculatorDisplay;
         };
     }
 }
