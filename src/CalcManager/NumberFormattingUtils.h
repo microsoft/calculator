c1fefd3a7b2f4b7161301bc9ce4d5880348dd20d
pi1024e <49824824+pi1024e@users.noreply.github.com>
2020-05-18 23:14:40 -0400

    Change precision and variables regarding the number of digits to be unsigned instead of signed (#1092)

--- src/CalcManager/NumberFormattingUtils.h
+++ src/CalcManager/NumberFormattingUtils.h
@@ -10,6 +10,6 @@ namespace CalcManager::NumberFormattingUtils
     void TrimTrailingZeros(_Inout_ std::wstring& input);
     unsigned int GetNumberDigits(std::wstring value);
     unsigned int GetNumberDigitsWholeNumberPart(double value);
-    std::wstring RoundSignificantDigits(double value, int numberSignificantDigits);
+    std::wstring RoundSignificantDigits(double value, unsigned int numberSignificantDigits);
     std::wstring ToScientificNumber(double number);
 }
