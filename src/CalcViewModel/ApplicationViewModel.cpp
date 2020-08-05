cacc6ecdea4a743775331b94db678f7622e19e8d
Pepe Rivera <joriv@microsoft.com>
2020-08-05 12:23:56 -0700

    Add extra check

--- src/CalcViewModel/ApplicationViewModel.cpp
+++ src/CalcViewModel/ApplicationViewModel.cpp
@@ -75,7 +75,7 @@ void ApplicationViewModel::Categories::set(IObservableVector<NavCategoryGroup ^>
 
 void ApplicationViewModel::Initialize(ViewMode mode)
 {
-    if (!NavCategory::IsValidViewMode(mode))
+    if (!NavCategory::IsValidViewMode(mode) || !NavCategory::IsViewModeEnabled(mode))
     {
         mode = ViewMode::Standard;
     }
