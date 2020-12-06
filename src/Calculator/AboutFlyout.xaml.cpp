f98f82171560bc6d5ef365e04fedf0fd27b8928d
Chaitanya Mehta <chaitanyamehta@outlook.com>
2020-12-06 02:58:00 -0500

    Add link to GitHub repo on about flyout

--- src/Calculator/AboutFlyout.xaml.cpp
+++ src/Calculator/AboutFlyout.xaml.cpp
@@ -40,6 +40,8 @@ AboutFlyout::AboutFlyout()
     auto copyrightText =
         LocalizationStringUtil::GetLocalizedString(resourceLoader->GetResourceString("AboutControlCopyright"), StringReference(to_wstring(BUILD_YEAR).c_str()));
     AboutControlCopyrightRun->Text = copyrightText;
+
+    InitializeContributeTextBlock();
 }
 
 void AboutFlyout::FeedbackButton_Click(_In_ Object ^ sender, _In_ RoutedEventArgs ^ e)
@@ -60,4 +62,31 @@ void AboutFlyout::SetVersionString()
 void AboutFlyout::SetDefaultFocus()
 {
     AboutFlyoutEULA->Focus(::FocusState::Programmatic);
+}
+
+void AboutFlyout::InitializeContributeTextBlock()
+{
+    auto resProvider = AppResourceProvider::GetInstance();
+    std::wstring contributeHyperlinkText = resProvider->GetResourceString(L"AboutFlyoutContribute")->Data();
+
+    // The resource string has the 'GitHub' hyperlink wrapped with '%HL%'.
+    // Break the string and assign pieces appropriately.
+    static const std::wstring delimiter{ L"%HL%" };
+    static const size_t delimiterLength{ delimiter.length() };
+
+    // Find the delimiters.
+    size_t firstSplitPosition = contributeHyperlinkText.find(delimiter, 0);
+    assert(firstSplitPosition != std::wstring::npos);
+    size_t secondSplitPosition = contributeHyperlinkText.find(delimiter, firstSplitPosition + 1);
+    assert(secondSplitPosition != std::wstring::npos);
+    size_t hyperlinkTextLength = secondSplitPosition - (firstSplitPosition + delimiterLength);
+
+    // Assign pieces.
+    auto contributeTextBeforeHyperlink = ref new String(contributeHyperlinkText.substr(0, firstSplitPosition).c_str());
+    auto contributeTextLink = ref new String(contributeHyperlinkText.substr(firstSplitPosition + delimiterLength, hyperlinkTextLength).c_str());
+    auto contributeTextAfterHyperlink = ref new String(contributeHyperlinkText.substr(secondSplitPosition + delimiterLength).c_str());
+
+    ContributeRunBeforeLink->Text = contributeTextBeforeHyperlink;
+    ContributeRunLink->Text = contributeTextLink;
+    ContributeRunAfterLink->Text = contributeTextAfterHyperlink;
 }
