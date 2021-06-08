1d345ee78c45bedffbbd95987e97ee58484249b7
Tian L <60599517+tian-lt@users.noreply.github.com>
2021-06-08 13:13:06 +0800

    [C# Calc] Reverts some changes for Currency constants (#1570)

--- src/CalcViewModel/DataLoaders/DataLoaderMockConstants.h
+++ src/CalcViewModel/DataLoaders/DataLoaderMockConstants.h
@@ -5,7 +5,7 @@
 
 namespace CalculatorApp
 {
-    namespace ViewModel::DataLoaders
+    namespace DataLoaders
     {
         static constexpr auto sc_MetadataUriLocalizeFor = L"https://go.microsoft.com/fwlink/?linkid=2091028&localizeFor=";
         static constexpr auto sc_RatiosUriRelativeTo = L"https://go.microsoft.com/fwlink/?linkid=2091307&localCurrency=";
