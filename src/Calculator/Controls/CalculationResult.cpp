a9e94187df7d4a035a73beb58ccd43c4425e7640
josepradaimasc <84806254+josepradaimasc@users.noreply.github.com>
2021-09-15 21:32:17 -0500

    modi

--- src/Calculator/Controls/CalculationResult.cpp
+++ src/Calculator/Controls/CalculationResult.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Licensed under the MIT License
+//  otro comentario.
 
 #include "pch.h"
 #include "CalculationResult.h"
