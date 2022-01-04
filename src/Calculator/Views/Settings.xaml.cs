4ebacda62d14814af3f83e24bbadc8433d9cb966
Matt Cooley <macool@microsoft.com>
2022-01-04 08:07:24 -0800

    Update build year in settings page to 2022 (#1770)

--- src/Calculator/Views/Settings.xaml.cs
+++ src/Calculator/Views/Settings.xaml.cs
@@ -23,9 +23,7 @@ namespace CalculatorApp
 {
     public sealed partial class Settings : UserControl
     {
-        // CSHARP_MIGRATION: TODO:
-        // BUILD_YEAR was a C++/CX macro and may update the value from the pipeline
-        private const string BUILD_YEAR = "2021";
+        private const string BUILD_YEAR = "2022";
 
         public event Windows.UI.Xaml.RoutedEventHandler BackButtonClick;
 
