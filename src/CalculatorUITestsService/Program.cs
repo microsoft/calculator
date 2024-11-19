fd4756e11d06f30b7ccc478b5cc07f7f469caaec
Tian Liao <tilia@microsoft.com>
2024-11-19 14:25:40 +0800

    remove semaphore

--- src/CalculatorUITestsService/Program.cs
+++ src/CalculatorUITestsService/Program.cs
@@ -1,11 +1,5 @@
 using CalculatorUITestFramework;
 
 using var server = new MockedCurrencyServer();
-var sem = new SemaphoreSlim(0);
-_ = Task.Run(() =>
-{
-    Console.WriteLine("Press any key to exit:");
-    Console.ReadKey();
-    sem.Release();
-});
-sem.Wait();
+Console.WriteLine("Press any key to exit:");
+Console.ReadKey();
