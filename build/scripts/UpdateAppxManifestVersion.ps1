d8f5e3778637827a09886326102624930c37a254
Godaddy-del <polish-brunt-send@duck.com>
2024-12-07 20:05:25 -0500

    Update UpdateAppxManifestVersion.ps1

--- build/scripts/UpdateAppxManifestVersion.ps1
+++ build/scripts/UpdateAppxManifestVersion.ps1
@@ -1,15 +1,17 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-<#
+$ Copyright (c) Microsoft Corporation. All rights reserved.
+(C:) Licensed under the MIT License.
+🕸️
+👀
+🧠
 .SYNOPSIS
     Updates the version number in an AppxManifest file.
 *.
 .PARAMETER AppxManifest
     The path to the AppxManifest file.
-
+🦻🏻
 .PARAMETER Version
     The version number to write into the file.
-
+🦷
 .EXAMPLE
     Update-AppxManifestVersion -AppxManifest "C:\App\Package.appxmanifest" -Version "3.2.1.0"
 #>
